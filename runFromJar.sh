3356d3f711b213219a56b68d61e672a9545ce48d
Patrizio Bertozzi <xpicio@gmail.com>
2026-01-11 15:46:16 +0100

    build: update scripts for JAR release

--- runFromJar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./gradlew shadowJar --no-daemon && \
-  cp build/libs/tetraj-all.jar tetraj.jar && \
-  java -jar tetraj.jar
---
+++ scripts/local-release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Local release script for testing the build process
+# Usage: ./scripts/local-release.sh [version]
+
+# Exit on error
+set -e
+
+VERSION=${1:-"1.0.0-SNAPSHOT"}
+echo "Building Tetraj release $VERSION"
+
+# Build shadowJar
+echo "Building shadow JAR..."
+./gradlew shadowJar --no-daemon
+
+# Copy JAR with version
+echo "Packaging JAR..."
+cp build/libs/tetraj-all.jar tetraj-$VERSION.jar
+cp build/libs/tetraj-all.jar tetraj.jar
+
+# Generate Javadoc
+echo "Generating Javadoc..."
+./gradlew javadoc --no-daemon
+
+# Prepare docs directory
+echo "Preparing documentation..."
+rm -rf docs
+cp -r build/docs/javadoc docs
